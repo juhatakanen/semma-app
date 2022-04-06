b722872849dd0ed124461ce1e90562d68c0b8039
Juha Takanen <juhatakanen@hotmail.com>
2022-04-06 14:48:43 +0300

    Heroku deployment changes

--- app.js
+++ app.js
@@ -154,6 +154,8 @@ function saltNumberF(food) {
   
   }
 
-app.listen(3000, () => {
-    console.log("server on 3000");
-})
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 3000;
+  }
+  app.listen(port);
